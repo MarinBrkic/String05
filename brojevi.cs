6e656111e09179e286f4d30e962f9b4facbf5e75
MarinBrkic <152974525+MarinBrkic@users.noreply.github.com>
2024-01-16 13:01:02 +0100

    Add files via upload

---
+++ brojevi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite jedan cijeli i jedan decimalni broj i razdvojite ih razmakom:");
+            string unos = Console.ReadLine();
+            string[] brojevi = unos.Split(' ');
+
+            int x = int.Parse(brojevi[0]);
+            float y = float.Parse(brojevi[1]);
+            x = x * 2;
+            y = y * 2;
+
+            Console.WriteLine("Cijeli: " + x);
+            Console.WriteLine("Decimalni: " + y);
+
+            Console.ReadKey();
+        }
+    }
+}
